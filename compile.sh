d4176788b172ec3dc7680b02dd6958bb2f4dd2c4
Eric Floehr <eric@intellovations.com>
2025-01-04 16:31:41 -0500

    Preparing infrastructure for hexprint

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
-arm-none-eabi-as -g -a=hellorld.list hellorld.s -o hellorld.o
-arm-none-eabi-ld -g hellorld.o -T./stm32f103rb.ld -o hellorld.elf
-arm-none-eabi-objcopy -O binary hellorld.elf hellorld.bin
+arm-none-eabi-as -g -a=hexprint.list hexprint.s -o hexprint.o
+arm-none-eabi-ld -g hexprint.o -T./stm32f103rb.ld -o hexprint.elf
+arm-none-eabi-objcopy -O binary hexprint.elf hexprint.bin
 # Can also use nm or readelf -S
-objdump -t hellorld.elf > hellorld.sym
-st-flash write hellorld.bin 0x8000000
+objdump -t hexprint.elf > hexprint.sym
+st-flash write hexprint.bin 0x8000000
--- debug.sh
+++ debug.sh
@@ -1,2 +1,2 @@
 openocd -f /usr/share/openocd/scripts/interface/stlink.cfg -f /usr/share/openocd/scripts/target/stm32f1x.cfg -f openocd-gdb.cfg
-# Then run gdb-multiarch hellorld.elf
+# Then run gdb-multiarch hexprint.elf
